5898169eba0d9bb8e966e4d97344506a50382a71
LUIGGIBB2022 <enlacevisualcis@gmail.com>
2024-03-24 23:18:57 -0500

    Actualización Archivos README.md

--- src/main/java/com/microservicioslitethinking/app/controller/UserController.java
+++ src/main/java/com/microservicioslitethinking/app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.microservicioslitethinking.app.controller;
 
 import com.microservicioslitethinking.app.entity.User;
 import com.microservicioslitethinking.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
     private final UserService userService;
-
+    @Autowired
     public UserController(UserService userService) {this.userService = userService;}
 
     @PostMapping("/user/create")
     public void  createUser(@RequestBody User user) {userService.createUser(user);}
+
+
+    @PostMapping("/user/update/{userId}")
+    public void  updateUser(@RequestBody User user, Long userId) {userService.updateUser(user,userId);}
 
 
 
--- src/main/java/com/microservicioslitethinking/app/service/UserService.java
+++ src/main/java/com/microservicioslitethinking/app/service/UserService.java
@@ -15,4 +15,22 @@ public class UserService {
 
     public void createUser (User user) { userRepo.save(user);}
 
+    public User updateUser (User userUpdate, Long userId) {
+        if (userRepo.existsById(userId))
+            {
+                // Obtener el usuario existente
+                User existingUser = userRepo.findById(userId).get();
+                // Realizar las  tareas de actualización del registro
+                existingUser.setName(userUpdate.getName());
+                existingUser.setPassword(userUpdate.getPassword());
+                existingUser.setPin(userUpdate.getPin());
+                return userRepo.save(existingUser);
+            }
+        else
+        {
+            return null;
+        }
+
+    }
+
 }
